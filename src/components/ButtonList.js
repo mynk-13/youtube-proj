006c82fab4d68c4fd3b4f566d6d67ff4e2736c48
mynk-13 <mynk13.projects@gmail.com>
2024-02-12 23:34:57 +0530

    Changed styling in Body component

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -5,7 +5,7 @@ const ButtonList = () => {
 
     const list=["All", "Stock Markets", "Cricket", "Live", "Gaming", "News","Cooking","Motivation", "Music", "Recently Uploaded"];
   return (
-    <div className='flex m-2 p-2'>
+    <div className='flex m-2 p-3'>
        {list?.map((data,index)=><Button key={index} name={data} />)}
     </div>
   )
