f9f9cfec691be8603e8ba19d177072f134f056cd
mynk-13 <mynk13.projects@gmail.com>
2024-03-17 12:27:14 +0530

    Added UI for a single comment

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Comment from './Comment';
+import { commentsData } from '../utils/constants';
 
 const CommentsContainer = () => {
+
+
+  
+
+
   return (
     <div className='m-5 p-2'>
         <h1 className='text-2xl font-bold'>Comments: </h1>
+        <Comment data={commentsData[0]} /> 
     </div>
   )
 }
