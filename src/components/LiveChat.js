ecdade9a842d8b21e7a9c194bbc6cf83864717ad
mynk-13 <mynk13.projects@gmail.com>
2024-03-23 19:52:55 +0530

    Added component ChatMessage to load each live chat comment

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import ChatMessage from './ChatMessage'
 
 const LiveChat = () => {
   return (
-    <div className='ml-2 w-full h-[500px] p-2 border border-black bg-slate-200 rounded-md'>LiveChat</div>
+    <div className='ml-2 w-full h-[500px] p-2 border border-slate-300 rounded-2xl'>
+       
+       <div className='my-2 py-1 px-1 font-medium border-b-2 border-b-slate-300'>
+         Live Chat 
+       </div>
+       <div className=''>
+       <ChatMessage name="Sanjay Choudhary" message="the music played is too good and wow never experienced such a movie before" />
+       <ChatMessage name="Sameer Raj" message="better than the last concert" />
+       <ChatMessage name="Mayank" message="sgdfxhacvdjknmlasdlasjkdldafshjakldasf lkjajdasfjks;aljdk;jsdsa;fjasljdkasjdkasjd" />
+       </div>
+       
+    </div>
   )
 }
 
