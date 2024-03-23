378bbb3a84f772a5f43f14dbd2962ed410ee1076
mynk-13 <mynk13.projects@gmail.com>
2024-03-24 01:01:13 +0530

    Added the feature to add the live comment by user and show in live chat using form

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatMessage from './ChatMessage'
 import { useDispatch, useSelector } from 'react-redux';
 import { addMessage } from '../utils/chatSlice';
@@ -7,6 +7,8 @@ import { generateRandomMessage, generateRandomName } from '../utils/helper';
 
 const LiveChat = () => {
 
+
+    const [liveUserMessage, setLiveUserMessage] = useState("");
 
     const dispatch=useDispatch();
 
@@ -21,7 +23,7 @@ const LiveChat = () => {
                 name: generateRandomName(),
                 message: generateRandomMessage(Math.floor(Math.random()*100)),
             }))
-        },500);
+        },1500);
 
         return ()=>clearInterval(id);
 
@@ -32,8 +34,9 @@ const LiveChat = () => {
 
 
   return (
-
-    <div className='ml-2 w-full h-[500px] px-2 pb-2 border border-slate-400 rounded-2xl'>
+    <>
+    
+     <div className='ml-2 w-full h-[570px] px-2 pb-2 border border-slate-400 rounded-2xl'>
        <div className='mt-0 pt-2 mb-2 pb-2 px-1 text-lg font-medium border-b-2 bg-white border-b-slate-300 sticky top-0 z-50'>
          Live Chat 
        </div> 
@@ -44,8 +47,31 @@ const LiveChat = () => {
 
        {liveChatMessages?.map((data,index)=> <ChatMessage key={index} name={data.name} message={data.message} />)}
        
-    </div>
-    </div>
+        </div>
+        <form className='flex my-3 py-2 border-t-2 border-slate-300' 
+        onSubmit={(e)=>{
+            e.preventDefault();
+            dispatch(addMessage({
+                name: "Mayank",
+                message: liveUserMessage
+            }))
+           
+         setLiveUserMessage("");
+        }}>
+            <input type="text"
+                   placeholder='Chat...' 
+                   value={liveUserMessage}
+                   onChange={(e)=>setLiveUserMessage(e.target.value)}
+                   className='ml-5 mr-1 my-2 w-10/12 bg-gray-200 border-gray-400 p-2 rounded-full' />
+            <button>
+                <img className='w-12 rounded-2xl'
+                     alt="send-btn"
+                     src="https://t4.ftcdn.net/jpg/01/42/65/07/360_F_142650708_u7qvychSOEQajI29PbEXr6VP7nAolb0V.jpg" />
+            </button>
+        </form>
+     </div>
+     
+    </>
     
   )
 }
