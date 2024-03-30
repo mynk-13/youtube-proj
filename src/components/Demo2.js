e0cd1da7a95bd4e2824733c5d63c9d0e8a38d396
mynk-13 <mynk13.projects@gmail.com>
2024-03-30 19:47:05 +0530

    Added another state variable to showcase useRef

--- src/components/Demo2.js
+++ src/components/Demo2.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Demo2 = () => {
 
     let x=10;
-
+    const [y, setY] = useState(0);
     // console.log(x);
   return (
     <div className='m-4 p-2 w-96 h-96 bg-slate-50 border border-black '>
@@ -14,6 +14,12 @@ const Demo2 = () => {
                 console.log(x)}
                 }>Increase x</button>
             <span className='font-bold text-xl'>Let = {x}</span>
+
+        </div>
+        <div>
+            <button className='bg-green-100 p-2  m-4' 
+             onClick={()=>setY(y+1)}>Increase y</button>
+            <span className='font-bold text-xl'>State = {y}</span>
 
         </div>
     </div>
