b25761cc49bc483996fe07a0feba707512939b88
mynk-13 <mynk13.projects@gmail.com>
2024-03-30 13:20:09 +0530

    Added another state variable dark theme to showcase memoization

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -4,23 +4,30 @@ import { findPrime } from '../utils/helper';
 const Demo = () => {
 
     const [text, setText]=useState("");
+    const [isDarkTheme, setIsDarkTheme] = useState(true);
 
-    console.log("Rendering....");
+    //console.log("Rendering....");
 
-    const prime = findPrime(text);
+    const prime =() => {
+         console.log("Calculating prime no of ", text); 
+        return findPrime(text);
+    }
 
     return (
-    <div className='m-4 p-2 w-96 h-96 border border-black'>
+    <div className={'m-4 p-2 w-96 h-96 border border-black ' + (isDarkTheme && "bg-black text-white")}>
+        <div>
+            <button className=' text-sm m-2 p-2 bg-green-400' onClick={()=>setIsDarkTheme(!isDarkTheme)}>{isDarkTheme?'Light': "Dark"}</button>
+        </div>
 
         <div>
-            <input type="text" 
-                   className='border border-black w-72 px-2'
+            <input type="number" 
+                   className={'border border-black w-72 px-2 ' + (isDarkTheme && "bg-gray-800 text-white")}
                    value={text} 
                    onChange={(e)=>setText(e.target.value)} />
             <button></button>
         </div>
         <div>
-            <h1>nth Prime: {prime} </h1>
+            <h1 className='mt-4 font-bold text-xl'>nth Prime: {prime()} </h1>
         </div>
 
     </div>
